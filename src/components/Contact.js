c01c714018f5ccb3a4b5601f8833efbc6a49f737
dorianwalker29 <119640290+dorianwalker29@users.noreply.github.com>
2023-07-22 22:35:31 -0400

    added contact and work page. with bug fixes and style changes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,57 @@
-// src/components/Contact.js
 import React from 'react';
 
 const Contact = () => {
   return (
-    <section className="py-16" id="contact">
+    <section className="py-16 bg-gray-800" id="contact">
       <div className="max-w-3xl mx-auto">
-        {/* Add your contact content here */}
-        <p>Contact</p>
+        <form className="w-full max-w-lg">
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full px-3">
+              <label className="block uppercase tracking-wide text-white text-xs font-bold mb-2" htmlFor="nick">
+                Name
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="nick"
+                type="text"
+              />
+              <p className="text-gray-600 text-xs italic"></p>
+            </div>
+          </div>
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full px-3">
+              <label className="block uppercase tracking-wide text-white text-xs font-bold mb-2" htmlFor="email">
+                E-mail
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="email"
+                type="email"
+              />
+              <p className="text-gray-600 text-xs italic"></p>
+            </div>
+          </div>
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full px-3">
+              <label className="block uppercase tracking-wide text-white text-xs font-bold mb-2" htmlFor="message">
+                Message
+              </label>
+              <textarea
+                className="no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
+                id="message"
+              ></textarea>
+              <p className="text-gray-600 text-xs italic"></p>
+            </div>
+          </div>
+          <div className="md:flex md:items-center">
+            <div className="md:w-1/3">
+              <button className="shadow bg-indigo-600 hover:bg-indigo-700 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+                Send
+              </button>
+            </div>
+            <div className="md:w-2/3"></div>
+          </div>
+        </form>
       </div>
     </section>
   );
--- src/components/Work.js
+++ src/components/Work.js
@@ -3,10 +3,73 @@ import React from 'react';
 
 const Work = () => {
   return (
-    <section className="py-16" id="work">
-      <div className="max-w-3xl mx-auto">
-        {/* Add your work content here */}
-        <p>Work Content</p>
+    <section className="py-16 bg-gray-800" id="work">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-4 mt-12 mb-12">
+        <h2 className="text-2xl font-extrabold text-white">My Work</h2>
+        <section className="mt-6 grid md:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-8">
+          {/* First Work */}
+          <article className="bg-white group relative rounded-lg overflow-hidden shadow-lg hover:shadow-2xl transform duration-200">
+            <div className="relative w-full h-80 md:h-64 lg:h-44">
+              <img src="https://cdn.pixabay.com/photo/2021/07/24/01/42/zebra-dove-6488440_960_720.jpg"
+                alt="Work 1"
+                className="w-full h-full object-center object-cover"
+              />
+            </div>
+            <div className="px-3 py-4">
+              <h3 className="text-sm text-gray-500 pb-2">
+                <a className="bg-indigo-600 py-1 px-2 text-white rounded-lg" href="#">
+                  <span className="absolute inset-0"></span>
+                  Basic Level
+                </a>
+              </h3>
+              <p className="text-base font-semibold text-gray-900 group-hover:text-indigo-600">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              </p>
+            </div>
+          </article>
+
+          {/* Second Work */}
+          <article className="bg-white group relative rounded-lg overflow-hidden shadow-lg hover:shadow-2xl transform duration-200">
+            <div className="relative w-full h-80 md:h-64 lg:h-44">
+              <img src="https://cdn.pixabay.com/photo/2021/09/08/20/45/bird-6607863_960_720.jpg.jpg"
+                alt="Work 2"
+                className="w-full h-full object-center object-cover"
+              />
+            </div>
+            <div className="px-3 py-4">
+              <h3 className="text-sm text-gray-500 pb-2">
+                <a className="bg-indigo-600 py-1 px-2 text-white rounded-lg" href="#">
+                  <span className="absolute inset-0"></span>
+                  Basic Level
+                </a>
+              </h3>
+              <p className="text-base font-semibold text-gray-900 group-hover:text-indigo-600">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              </p>
+            </div>
+          </article>
+
+          {/* Third Work */}
+          <article className="bg-white group relative rounded-lg overflow-hidden shadow-lg hover:shadow-2xl transform duration-200">
+            <div className="relative w-full h-80 md:h-64 lg:h-44">
+              <img src="https://cdn.pixabay.com/photo/2021/08/03/11/01/stairs-6519085_960_720.jpg"
+                alt="Work 3"
+                className="w-full h-full object-center object-cover"
+              />
+            </div>
+            <div className="px-3 py-4">
+              <h3 className="text-sm text-gray-500 pb-2">
+                <a className="bg-indigo-600 py-1 px-2 text-white rounded-lg" href="#">
+                  <span className="absolute inset-0"></span>
+                  Intermediate Level
+                </a>
+              </h3>
+              <p className="text-base font-semibold text-gray-900 group-hover:text-indigo-600">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              </p>
+            </div>
+          </article>
+        </section>
       </div>
     </section>
   );
